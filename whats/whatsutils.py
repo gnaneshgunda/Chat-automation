d0651750c0b1e335a68a86ced799f7b67e11e460
Durga Sai <dsainvg.20.12.24@kgpian.iitkgp.ac.in>
2025-06-16 16:00:22 +0530

    Fix import statement for logging in whatsutils.py

--- whats/whatsutils.py
+++ whats/whatsutils.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import logging
 
 import time
 import pandas as pd
